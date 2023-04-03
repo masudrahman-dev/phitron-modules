b601a82b0caa430bc49cb662e6a180caa4f14d59
masudrahman-dev <masudrahman.dev@gmail>
2023-04-03 09:45:04 +0600

    array

--- phitron-modules/modules-1/variable.c
+++ phitron-modules/modules-1/variable.c
@@ -328,44 +328,71 @@
 //     else
 //         printf("%d is a prime number.", n);
 // }
+// #include <stdio.h>
+// int checkPrimeNumber(int n);
+
+// int main()
+// {
+
+//     int n, flag;
+
+//     printf("Enter a positive integer: ");
+//     scanf("%d", &n);
+
+//     // n is passed to the checkPrimeNumber() function
+//     // the returned value is assigned to the flag variable
+//     flag = checkPrimeNumber(n);
+
+//     if (flag == 1)
+//         printf("%d is not a prime number", n);
+//     else
+//         printf("%d is a prime number", n);
+
+//     return 0;
+// }
+
+// // int is returned from the function
+// int checkPrimeNumber(int n)
+// {
+
+//     // 0 and 1 are not prime numbers
+//     if (n == 0 || n == 1)
+//         return 1;
+
+//     int i;
+
+//     for (i = 2; i <= n / 2; ++i)
+//     {
+//         if (n % i == 0)
+//             return 1;
+//     }
+
+//     return 0;
+// }
+// Program to take 5 values from the user and store them in an array
+// Print the elements stored in the array
+
 #include <stdio.h>
-int checkPrimeNumber(int n);
 
 int main()
 {
 
-    int n, flag;
+    int values[5];
 
-    printf("Enter a positive integer: ");
-    scanf("%d", &n);
+    printf("Enter 5 integers: \n");
 
-    // n is passed to the checkPrimeNumber() function
-    // the returned value is assigned to the flag variable
-    flag = checkPrimeNumber(n);
-
-    if (flag == 1)
-        printf("%d is not a prime number", n);
-    else
-        printf("%d is a prime number", n);
-
-    return 0;
-}
-
-// int is returned from the function
-int checkPrimeNumber(int n)
-{
-
-    // 0 and 1 are not prime numbers
-    if (n == 0 || n == 1)
-        return 1;
-
-    int i;
-
-    for (i = 2; i <= n / 2; ++i)
+    // taking input and storing it in an array
+    for (int i = 0; i < 5; ++i)
     {
-        if (n % i == 0)
-            return 1;
+        scanf("%d", &values[i]);
     }
 
+    printf("Displaying integers: ");
+
+    // printing elements of an array
+    for (int i = 0; i < 5; ++i)
+    {
+        printf("%d\n", values[i]);
+    }
     return 0;
 }
