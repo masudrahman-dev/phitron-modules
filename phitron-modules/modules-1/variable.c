e432f06bf2afb0c2450545a6e903170338be63b3
masudrahman-dev <masudrahman.dev@gmail>
2023-04-03 12:20:40 +0600

    memory allocation

--- phitron-modules/modules-1/variable.c
+++ phitron-modules/modules-1/variable.c
@@ -492,22 +492,64 @@
 
 
 
-#include <stdio.h>
 
-int main()
-{
-    int tk;
 
-    scanf("%d", &tk);
 
-    if (tk == 100)
-    {
-        printf("burger khamu\n");
-    }
-    else
-    {
-        printf("burger khamu na\n");
-    }
+
+
+
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int tk;
+
+//     scanf("%d", &tk);
+
+//     if (tk == 100)
+//     {
+//         printf("burger khamu\n");
+//     }
+//     else
+//     {
+//         printf("burger khamu na\n");
+//     }
  
-    return 0;
+//     return 0;
+// }
+
+
+// Program to calculate the sum of n numbers entered by the user
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n, i, *ptr, sum = 0;
+
+  printf("Enter number of elements: ");
+  scanf("%d", &n);
+
+  ptr = (int*) malloc(n * sizeof(int));
+ 
+  // if memory cannot be allocated
+  if(ptr == NULL) {
+    printf("Error! memory not allocated.");
+    exit(0);
+  }
+
+  printf("Enter elements: ");
+  for(i = 0; i < n; ++i) {
+    scanf("%d", ptr + i);
+    sum += *(ptr + i);
+  }
+
+  printf("Sum = %d", sum);
+  
+  // deallocating the memory
+  free(ptr);
+
+  return 0;
 }
