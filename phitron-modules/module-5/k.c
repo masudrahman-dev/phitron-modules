0021102440a6b22f2d44bd159a6e06714e670f13
masudrahman-dev <masudrahman.dev@gmail>
2023-04-09 18:29:20 +0600

    min max done

--- phitron-modules/module-5/k.c
+++ phitron-modules/module-5/k.c
@@ -1,33 +1,68 @@
+// #include <stdio.h>
+// int main()
+// {
+//     double a, b, c, max, min;
+//     scanf("%d %d %d", &a, &b, &c);
+//     if (a > b && a > c)
+//     {
+//         max = a;
+//     }
+//     else if (b > a && b > c)
+//     {
+//         max = b;
+//     }
+//     else
+//     {
+//         max = c;
+//     }
+//     if (a < b && a < c)
+//     {
+//         min = a;
+//     }
+//     else if (b < a && b < c)
+//     {
+//         min = b;
+//     }
+//     else
+//     {
+//         min = c;
+//     }
+//     printf("%d %d", min, max);
+//     // printf("%d\n",min);
+//     return 0;
+// }
+
 #include <stdio.h>
+
 int main()
 {
-    double a, b, c, max, min;
+    int a, b, c, max, min;
+
     scanf("%d %d %d", &a, &b, &c);
-    if (a > b && a > c)
-    {
-        max = a;
-    }
-    else if (b > a && b > c)
+
+    // Find maximum number
+    max = a;
+    if (b > max)
     {
         max = b;
     }
-    else
+    if (c > max)
     {
         max = c;
     }
-    if (a < b && a < c)
-    {
-        min = a;
-    }
-    else if (b < a && b < c)
+
+    // Find minimum number
+    min = a;
+    if (b < min)
     {
         min = b;
     }
-    else
+    if (c < min)
     {
         min = c;
     }
+
     printf("%d %d", min, max);
-    // printf("%d\n",min);
+
     return 0;
 }
