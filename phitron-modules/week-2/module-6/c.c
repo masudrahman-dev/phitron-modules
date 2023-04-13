cd347fa3ba926f4772b1b953813b0a65407b50da
masudrahman-dev <masudrahman.dev@gmail>
2023-04-13 09:16:50 +0600

    week 2

---
+++ phitron-modules/week-2/module-6/c.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int main()
+{
+    int even = 0, odd = 0, positive = 0, negative = 0, n, m;
+    // scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        // scanf("%d %d %d %d", &a, &b, &c, &d);
+        scanf("%d", &m);
+        if (m % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+        if (m >= 0)
+        {
+            positive++;
+        }
+        else
+        {
+            negative--;
+        }
+    }
+
+    // for (int i = 1; i < n; i++)
+    // {
+    //     if (i % 2 == 0)
+    //     {
+    //         even++;
+    //     }
+    //     else
+    //     {
+    //         odd++;
+    //     }
+    //     if (n >= 0)
+    //     {
+    //         positive++;
+    //     }
+    //     else
+    //     {
+    //         negative--;
+    //     }
+    // }
+
+    printf(" Even: %d\n", even);
+    printf(" Odd: %d\n", odd);
+    printf(" Positive: %d\n", positive);
+    printf(" Negative: %d\n", negative);
+    return 0;
+}
