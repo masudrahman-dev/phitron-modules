3b123cb892e2f230d71a3d0c47ef3f48006a59ec
masudrahman-dev <masudrahman.dev@gmail>
2023-04-19 07:56:27 +0600

    d no does not solve

--- phitron-modules/week-3/module-10.5-practice-day-1/D_Strings.c
+++ phitron-modules/week-3/module-10.5-practice-day-1/D_Strings.c
@@ -24,6 +24,44 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+// #include <string.h>
+
+// #define MAX_LENGTH 11
+
+// int main()
+// {
+//     char str1[MAX_LENGTH];
+//     char str2[MAX_LENGTH];
+//     char temp;
+
+//     // Reading the input strings
+//     // printf("Enter the first string (A): ");
+//     fgets(str1, sizeof(str1), stdin);
+
+//     // printf("Enter the second string (B): ");
+//     fgets(str2, sizeof(str2), stdin);
+
+//     // Removing the newline character from the end of the strings
+//     str1[strcspn(str1, "\n")] = '\0';
+//     str2[strcspn(str2, "\n")] = '\0';
+
+//     // Printing the size of the strings
+//     printf("%ld %ld\n", strlen(str1), strlen(str2));
+
+//     // Concatenating the two strings
+//     strcat(str1, str2);
+//     printf("%s\n", str1);
+
+//     // Swapping the first characters of the two strings
+//     temp = str1[0];
+//     str1[0] = str2[0];
+//     str2[0] = temp;
+
+//     printf("%s %s", str1, str2);
+
+//     return 0;
+// }
 #include <stdio.h>
 #include <string.h>
 
@@ -33,13 +71,14 @@ int main()
 {
     char str1[MAX_LENGTH];
     char str2[MAX_LENGTH];
+    char str3[MAX_LENGTH ];
     char temp;
 
     // Reading the input strings
-    printf("Enter the first string (A): ");
+    // printf("Enter the first string (A): ");
     fgets(str1, sizeof(str1), stdin);
 
-    printf("Enter the second string (B): ");
+    // printf("Enter the second string (B): ");
     fgets(str2, sizeof(str2), stdin);
 
     // Removing the newline character from the end of the strings
@@ -47,18 +86,18 @@ int main()
     str2[strcspn(str2, "\n")] = '\0';
 
     // Printing the size of the strings
-    printf("Size of A and B: %ld %ld\n", strlen(str1), strlen(str2));
+    printf("%d %d\n", strlen(str1), strlen(str2));
 
-    // Concatenating the two strings
-    strcat(str1, str2);
-    printf("Concatenated string: %s\n", str1);
 
+    sprintf(str3, "%s%s", str1, str2);
+    printf("%s\n", str3);
     // Swapping the first characters of the two strings
     temp = str1[0];
     str1[0] = str2[0];
     str2[0] = temp;
 
-    printf("Swapped strings: %s %s\n", str1, str2);
+    // Printing the swapped strings, starting from the second character of str1
+    printf("%s %s", str1, str2);
 
     return 0;
 }
