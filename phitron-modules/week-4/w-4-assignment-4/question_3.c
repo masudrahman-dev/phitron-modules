f368c5ada0becd34cfd4ae1f4cefa84e4be28582
masudrahman-dev <masudrahman.dev@gmail>
2023-05-11 20:58:32 +0600

    asignment re take

--- phitron-modules/week-4/w-4-assignment-4/question_3.c
+++ phitron-modules/week-4/w-4-assignment-4/question_3.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 
-void increment(int x) {
-    x++;
+int count_before_zero(int arr[], int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == 0)
+        {
+            break;
+        }
+        count++;
+    }
+    return count;
 }
 
-int main() {
-    int num = 5;
-    increment(num);
-    printf("%d\n", num); 
-    return 0;
-}
+int main()
+{
+    int n;
+    int arr[n];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
 
-
-#include <stdio.h>
-
-void increment(int *x) {
-    (*x)++;
-}
-
-int main() {
-    int num = 5;
-    increment(&num);
-    printf("%d\n", num); 
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int count = count_before_zero(arr, size);
+    printf(" %d\n", count);
     return 0;
 }
