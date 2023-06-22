6bc5ece930a26e68c0fe5f07b04b26233fee2870
masudrahman-dev <masudrahman.dev@gmail>
2023-06-22 08:44:00 +0600

    insert at any position

--- Modules/Basic-Data-Structure/Assignment-2/insert_any_postion.cpp
+++ Modules/Basic-Data-Structure/Assignment-2/insert_any_postion.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 class Node
@@ -6,12 +6,14 @@ class Node
 public:
     int data;
     Node *next;
+
     Node(int data)
     {
         this->data = data;
         this->next = NULL;
     }
 };
+
 void insert_at_tail(Node *&head, int val)
 {
     Node *newNode = new Node(val);
@@ -25,13 +27,41 @@ void insert_at_tail(Node *&head, int val)
     {
         temp = temp->next;
     }
-    // temp last node e ache
     temp->next = newNode;
 }
+
+void insert_at_head(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+
+void insert_any_pos(Node *&head, int val, int pos)
+{
+    if (pos == 0)
+    {
+        insert_at_head(head, val);
+        return;
+    }
+    Node *newNode = new Node(val);
+    Node *temp = head;
+    for (int i = 0; i < pos - 1; i++)
+    {
+        if (temp == NULL)
+        {
+            cout << "Invalid position!" << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
 void print_list(Node *head)
 {
     Node *temp = head;
-
     while (temp != NULL)
     {
         cout << temp->data << " ";
@@ -40,6 +70,7 @@ void print_list(Node *head)
     cout << endl
          << endl;
 }
+
 int main()
 {
     Node *head = NULL;
@@ -49,7 +80,7 @@ int main()
         cout << "Option 0: Terminate" << endl;
         cout << "Option 1: Insert at Tail" << endl;
         cout << "Option 2: Insert at Head" << endl;
-        cout << "Option 3: Insert Any Position" << endl;
+        cout << "Option 3: Insert at Any Position" << endl;
         cout << "Option 9: Print List" << endl;
 
         int opt;
@@ -61,10 +92,27 @@ int main()
         }
         else if (opt == 1)
         {
-            cout << "inset value: ";
+            cout << "Insert value: ";
             int val;
             cin >> val;
             insert_at_tail(head, val);
+        }
+        else if (opt == 2)
+        {
+            cout << "Insert value: ";
+            int val;
+            cin >> val;
+            insert_at_head(head, val);
+        }
+        else if (opt == 3)
+        {
+            cout << "Insert value: ";
+            int val;
+            cin >> val;
+            cout << "Insert position: ";
+            int pos;
+            cin >> pos;
+            insert_any_pos(head, val, pos);
         }
         else if (opt == 9)
         {
@@ -72,14 +120,5 @@ int main()
         }
     }
 
-    // if (opt == 1)
-    // {
-    //     int val;
-    //     while (cin >> val, val != -1)
-    //     {
-    //         insert_at_tail(head, val);
-    //     }
-    // }
-    // print_list(head);
     return 0;
 }
