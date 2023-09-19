dc7fb669cd8821a664238e35bda305a6173a7af4
masud-rahman-mvn <masudur.rahman@moveon.com.bd>
2023-09-20 02:10:05 +0600

    algorithms final exam done

--- Modules-Phitron/algorithm/week-7/Mina_and_Rita.cpp
+++ Modules-Phitron/algorithm/week-7/Mina_and_Rita.cpp
@@ -1,43 +1,39 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int shortestCombinedLength(string A, string B) {
-    int lenA = A.length();
-    int lenB = B.length();
-    
-    // Initialize a 2D vector to store the longest common suffix length
-    vector<vector<int>> dp(lenA + 1, vector<int>(lenB + 1, 0));
-
-    // Fill the dp table to find the longest common suffix length
-    for (int i = 1; i <= lenA; i++) {
-        for (int j = 1; j <= lenB; j++) {
-            if (A[i - 1] == B[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            }
-        }
-    }
-
-    // Calculate the combined length of both strings minus the common suffix
-    int combinedLength = lenA + lenB - (2 * dp[lenA][lenB]);
-    return combinedLength;
-}
 
 int main() {
     int T;
     cin >> T;
-    cin.ignore(); // Consume the newline character after T
-
+    
     while (T--) {
-        string A, B;
-        getline(cin, A);
-        getline(cin, B);
-
-        int result = shortestCombinedLength(A, B);
-        cout << result << endl;
+        string a, b;
+        cin >> a >> b;
+        int n = a.size();
+        int m = b.size();
+        int dp[n + 1][m + 1];
+        
+        // Initialize the dp array
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+        
+        // Calculate the SCS using dynamic programming
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a[i - 1] == b[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        // The length of the SCS is (n + m - dp[n][m])
+        cout << n + m - dp[n][m] << endl;
     }
-
+    
     return 0;
 }
